22e24f9253ad8ad401f1c153c649077b22dc0326
anhluuvt <anhluuviettuan@gmail.com>
2022-10-12 10:43:48 +0700

    1st

---
+++ service/article_service/article.go
@@ -0,0 +1,149 @@
+package article_service
+
+import (
+	"encoding/json"
+
+	"github.com/anhluuvt/gin-demo/models"
+	"github.com/anhluuvt/gin-demo/pkg/e"
+	"github.com/anhluuvt/gin-demo/pkg/gredis"
+	"github.com/anhluuvt/gin-demo/pkg/logging"
+	"github.com/anhluuvt/gin-demo/service/cache_service"
+)
+
+type Article struct {
+	ID            int
+	TagID         int
+	Title         string
+	Desc          string
+	Content       string
+	CoverImageUrl string
+	State         int
+	CreatedBy     string
+	ModifiedBy    string
+
+	PageNum  int
+	PageSize int
+}
+
+func (a *Article) Add() error {
+	err := gredis.LikeDeletes(e.CACHE_ARTICLE)
+	if err != nil {
+		logging.Info(err)
+	}
+	article := map[string]interface{}{
+		"tag_id":          a.TagID,
+		"title":           a.Title,
+		"desc":            a.Desc,
+		"content":         a.Content,
+		"created_by":      a.CreatedBy,
+		"cover_image_url": a.CoverImageUrl,
+		"state":           a.State,
+	}
+
+	if err := models.AddArticle(article); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *Article) Edit() error {
+	err := gredis.LikeDeletes(e.CACHE_ARTICLE)
+	if err != nil {
+		logging.Info(err)
+	}
+	return models.EditArticle(a.ID, map[string]interface{}{
+		"tag_id":          a.TagID,
+		"title":           a.Title,
+		"desc":            a.Desc,
+		"content":         a.Content,
+		"cover_image_url": a.CoverImageUrl,
+		"state":           a.State,
+		"modified_by":     a.ModifiedBy,
+	})
+}
+
+func (a *Article) Get() (*models.Article, error) {
+	var cacheArticle *models.Article
+
+	cache := cache_service.Article{ID: a.ID}
+	key := cache.GetArticleKey()
+	if gredis.Exists(key) {
+		data, err := gredis.Get(key)
+		if err != nil {
+			logging.Info(err)
+		} else {
+			json.Unmarshal(data, &cacheArticle)
+			return cacheArticle, nil
+		}
+	}
+
+	article, err := models.GetArticle(a.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	gredis.Set(key, article, 3600)
+	return article, nil
+}
+
+func (a *Article) GetAll() ([]*models.Article, error) {
+	var (
+		articles, cacheArticles []*models.Article
+	)
+
+	cache := cache_service.Article{
+		TagID: a.TagID,
+		State: a.State,
+
+		PageNum:  a.PageNum,
+		PageSize: a.PageSize,
+	}
+	key := cache.GetArticlesKey()
+	if gredis.Exists(key) {
+		data, err := gredis.Get(key)
+		if err != nil {
+			logging.Info(err)
+		} else {
+			json.Unmarshal(data, &cacheArticles)
+			return cacheArticles, nil
+		}
+	}
+
+	articles, err := models.GetArticles(a.PageNum, a.PageSize, a.getMaps())
+	if err != nil {
+		return nil, err
+	}
+
+	gredis.Set(key, articles, 3600)
+	return articles, nil
+}
+
+func (a *Article) Delete() error {
+	err := gredis.LikeDeletes(e.CACHE_ARTICLE)
+	if err != nil {
+		logging.Info(err)
+	}
+	return models.DeleteArticle(a.ID)
+}
+
+func (a *Article) ExistByID() (bool, error) {
+	return models.ExistArticleByID(a.ID)
+}
+
+func (a *Article) Count() (int, error) {
+	return models.GetArticleTotal(a.getMaps())
+}
+
+func (a *Article) getMaps() map[string]interface{} {
+	maps := make(map[string]interface{})
+	maps["deleted_on"] = 0
+	if a.State != -1 {
+		maps["state"] = a.State
+	}
+	if a.TagID != -1 {
+		maps["tag_id"] = a.TagID
+	}
+
+	return maps
+}
---
+++ service/auth_service/auth.go
@@ -0,0 +1,12 @@
+package auth_service
+
+import "github.com/anhluuvt/gin-demo/models"
+
+type Auth struct {
+	Username string
+	Password string
+}
+
+func (a *Auth) Check() (bool, error) {
+	return models.CheckAuth(a.Username, a.Password)
+}
